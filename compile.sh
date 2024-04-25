4d9f63749ccfdc6f904312ae0dd235950fe0a28c
HECK <59280080+kayoStander@users.noreply.github.com>
2024-04-25 20:38:36 -0300

    Delete compile.sh

--- compile.sh
+++
@@ -1,3 +0,0 @@
-../vulkanSDK/x86_64/bin/glslc shaders/simple_shader.vert -o shaders/simple_shader.vert.spv
-../vulkanSDK/x86_64/bin/glslc shaders/simple_shader.frag -o shaders/simple_shader.frag.spv
-
